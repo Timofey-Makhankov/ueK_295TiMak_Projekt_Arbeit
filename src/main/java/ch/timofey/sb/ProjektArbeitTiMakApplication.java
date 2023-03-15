f214dd4d647261c10e452230c3cdbcb35bce06eb
Timofey-Makhankov <timofey.makhankov@noseryoung.com>
2023-03-15 14:18:24 +0100

    init Project

---
+++ src/main/java/ch/timofey/sb/ProjektArbeitTiMakApplication.java
@@ -0,0 +1,13 @@
+package ch.timofey.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektArbeitTiMakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektArbeitTiMakApplication.class, args);
+	}
+
+}
